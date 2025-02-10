004c72d289cfcc81adfdda803f571ff2c75a47e9
Koularis <studykoularis@gmail.com>
2025-02-10 17:02:31 +0200

    feat: create main menu

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Rock Paper Scissors
 
 Simple website where you can play Rock Paper Scissors versus an AI
+made in React.
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,50 @@
 import { useState } from 'react'
+import rock from './assets/icons8-rock-100.png';
+import paper from './assets/icons8-paper-100.png';
+import scissors from './assets/icons8-scissors-100.png';
 import './App.css'
 
-function App() {
+function GameBox() {
+  return (
+    <div className='gameContainer'>
+      <img src={rock} alt='rock' id='rock'></img>
+      <img src={paper} alt='paper' id='paper'></img>
+      <img src={scissors} alt='scissors' id='scissors'></img>
+    </div>
+  );
+}
 
+function Welcome({setStatus}){
+  return(
+    <input type='button' value='Start' onClick={() =>setStatus(true)}></input>
+  )
+}
+
+function Scorebox(){
+  return(
+    <div>
+      <p>0 - 0</p>
+    </div>
+  )
+}
+
+
+
+function App() {
+  
+  const [hasStarted,setStatus] = useState(false);
   return (
     <>
       <h1>Rock Paper Scissors</h1>
+      {(!hasStarted) ?
+      <Welcome setStatus={setStatus}/> :
+      (<div>
+        <GameBox/>
+        <Scorebox/>
+      </div>)
+      }
     </>
-  )
+  );
 }
 
 export default App
