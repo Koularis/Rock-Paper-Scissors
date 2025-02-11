0d6030147547d36e0750e99935519acbe712df01
Koularis <studykoularis@gmail.com>
2025-02-11 15:56:55 +0200

    feat:complete game round logic with score updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
 import { useState } from 'react'
-import rock from './assets/icons8-rock-100.png';
-import paper from './assets/icons8-paper-100.png';
-import scissors from './assets/icons8-scissors-100.png';
-import gameRound from './gameLogic';
+import GameBox from './gameLogic';
 import './App.css'
 
 
-function GameBox() {
-  return (
-    <div className='gameContainer'>
-      <img src={rock} alt='rock' id='rock' onClick={() => gameRound('rock')}></img>
-      <img src={paper} alt='paper' id='paper' onClick={() => gameRound('paper')}></img>
-      <img src={scissors} alt='scissors' id='scissors' onClick={() => gameRound('scissors')}></img>
-    </div>
-  );
-}
+
 
 function Welcome({setStatus}){
   return(
@@ -22,20 +11,9 @@ function Welcome({setStatus}){
   )
 }
 
-function Scorebox({score}){
-  const[playerScore, computerScore] = score
-  return(
-    <div>
-      <p>{playerScore} - {computerScore}</p>
-    </div>
-  )
-}
-
-
 
 function App() {
   
-  const [score, setScore] = useState([0, 0]);
   const [hasStarted,setStatus] = useState(false);
   
   return (
@@ -45,7 +23,6 @@ function App() {
       <Welcome setStatus={setStatus}/> :
       (<div>
         <GameBox/>
-        <Scorebox score={score}/>
      </div>)
       }
     </>
--- src/gameLogic.jsx
+++ src/gameLogic.jsx
@@ -1,39 +1,81 @@
+import { useState } from 'react';
+import rock from './assets/icons8-rock-100.png';
+import paper from './assets/icons8-paper-100.png';
+import scissors from './assets/icons8-scissors-100.png';
+
 function getComputerChoice(){
     const choices = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * 3);
     return choices[randomIndex];
 }
 
-export default function gameRound(playerSelection){
-    const computerSelection = getComputerChoice();
-    console.log(computerSelection);
-    switch(computerSelection){
-        case 'rock':
-            if(playerSelection === 'paper'){
-                return 'You Win! Paper beats Rock';
-            }
-            else if(playerSelection === 'scissors'){
-                return 'You Lose! Rock beats Scissors';
-            }
-            else{
-                return 'Draw';
-            }
-        case 'paper':
-            if(playerSelection === 'scissors'){
-                return 'You Win! Scissors beats Paper';
-            }
-            else if(playerSelection === 'rock'){
-                return 'You Lose! Paper beats Rock';
-            }
-            else{
-                return 'Draw';
-            }
-        case 'scissors':
-            if(playerSelection === 'rock'){
-                return 'You Win! Rock beats Scissors';
-            }
-            else if(playerSelection === 'paper'){
-                return 'You Lose! Scissors beats Paper';
-            }
+
+function Scorebox({score}){
+    const[playerScore, computerScore] = score
+    return(
+      <div>
+        <p>{playerScore} - {computerScore}</p>
+      </div>
+    )
+  }
+
+
+export default function GameBox() {
+    const [score, setScore] = useState([0, 0]);
+    function gameRound(playerSelection){
+        const[playerScore, computerScore] = score
+        const computerSelection = getComputerChoice();
+        switch(computerSelection){
+            case 'rock':
+                if(playerSelection === 'paper'){
+                    setScore([playerScore + 1, computerScore])
+                    alert(`You win! Computer picked ${computerSelection}`);
+                }
+                else if(playerSelection === 'scissors'){
+                    setScore([playerScore, computerScore + 1])
+                    alert(`You lose! Computer picked ${computerSelection}`);
+                }
+                else{
+                    alert('Draw');
+                }
+                break;
+            case 'paper':
+                if(playerSelection === 'scissors'){
+                    setScore([playerScore + 1, computerScore])
+                    alert(`You win! Computer picked ${computerSelection}`);
+                }
+                else if(playerSelection === 'rock'){
+                    setScore([playerScore, computerScore + 1])
+                    alert(`You lose! Computer picked ${computerSelection}`);
+                }
+                else{
+                    alert('Draw');
+                }
+                break;
+            case 'scissors':
+                if(playerSelection === 'rock'){
+                    setScore([playerScore + 1, computerScore])
+                    alert(`You win! Computer picked ${computerSelection}`);
+                }
+                else if(playerSelection === 'paper'){
+                    setScore([playerScore, computerScore + 1])
+                    alert(`You lose! Computer picked ${computerSelection}`);
+                }
+                else{
+                    alert('Draw');
+                }
+                break;
+        }
     }
-}
+
+    return (
+      <>
+        <div className='gameContainer'>
+          <img src={rock} alt='rock' id='rock' onClick={() => gameRound('rock')}></img>
+          <img src={paper} alt='paper' id='paper' onClick={() => gameRound('paper')}></img>
+          <img src={scissors} alt='scissors' id='scissors' onClick={() => gameRound('scissors')}></img>
+        </div>
+        < Scorebox score={score}/>
+      </>
+    );
+  }
